f965190ca112216476615da504dcc9826be6cbd0
Jarno Rankinen <50285623+0ranki@users.noreply.github.com>
2024-06-05 16:56:57 +0300

    Install zfs rpms from ucore-kmods

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ RELEASE="$(rpm -E %fedora)"
 
 # this installs a package from fedora repos
 rpm-ostree install screen
+rpm-ostree install /tmp/rpms/*.rpm
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
