3154003806765a9c9a64ca1169e5959d5b2c8036
Jarno Rankinen <50285623+0ranki@users.noreply.github.com>
2024-06-05 17:09:10 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,8 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
-rpm-ostree install /tmp/rpms/kmod/zfs/*.rpm
+rpm-ostree install /tmp/rpms/kmods/zfs/*.rpm
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
